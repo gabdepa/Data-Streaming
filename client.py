e00be476e5eb5ea992903b309d53c61907a46218
gabdepa <gabriel-r-p-p@hotmail.com>
2023-10-22 17:22:42 -0300

    Almost done

--- client.py
+++ client.py
@@ -1,41 +1,54 @@
 import socket
 import json
 
+# server_address = str(input("Enter the server address:"))
+server_address = "127.0.0.1"
 
-server_address = input("Enter the server address:")
-
-# Inicialize o socket UDP
+# Inicialização do socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.sendto("register".encode(), (str(server_address), 12345))
+client_socket.sendto("register".encode(), (server_address, 12345))
 
-received_packets = 0
-lost_packets = 0
-out_of_order = 0
-last_received = 0
+lost_packets = 0 # Número de pacotes perdidos
+out_of_order = 0 # Número de pacotes fora de ordem
+last_received = 0 # Número do último pacote recebido
+received_packets = 0  # Contador de pacotes recebidos
+first_packet_received = True  # Flag para verificar se é o primeiro pacote recebido
 
-while True:
-   # Recebe mensagem e endereço do socket
-    message, address = client_socket.recvfrom(1024)
-    
-    # Converte a mensagem JSON para um dicionário Python
-    message_dict = json.loads(message.decode())
-    print(f"(client) Received: {message_dict} from {address}")
+try:
+    while True:
+        # Recebe mensagem e endereço do socket
+        message, address = client_socket.recvfrom(1024)
+        
+        # Converte a mensagem JSON para um dicionário Python
+        message_dict = json.loads(message.decode())
+        
+        # Acessa o contador da mensagem
+        current_order = message_dict["message"]["count"]
+        if first_packet_received:
+            # Se for o primeiro pacote, inicialize o contador e defina a flag para False
+            first_packet_received = False
+            received_packets = 1        
+            last_received = current_order
+        else:
+            # Se não for o primeiro pacote, incremente o contador
+            received_packets += 1
+        
+        # Estatísticas (simples)
+        if current_order < last_received:
+            out_of_order += 1
+        elif current_order > last_received + 1:
+            lost_packets += (current_order - last_received - 1)
+        
+        # Implementar alguma operação nos dados recebidos
+        print(f"(client) Received: {message_dict} from {address}") 
+        # Dados de streaming
+        print(f"(client) number of packets received: {received_packets}")
+        print(f"(client) number of lost packets: {lost_packets}")
+        print(f"(client) number of packets out of order: {out_of_order}\n")
+        
+        # Atualize o último pacote recebido
+        last_received = current_order
 
-    # Contador de pacotes
-    received_packets += 1
-    
-    # Implementar alguma operação nos dados recebidos
-    # Por exemplo, imprimir a mensagem
-    print(f"(client) Received: {message_dict} from {address}")
-    
-    # Estatísticas (simples)
-    # Agora, use os campos do dicionário para estatísticas ou qualquer outra operação
-    current_order = message_dict["message"]["count"]  # Aqui estamos acessando o campo "count" do dicionário
-    if current_order < last_received:
-        out_of_order += 1
-    elif current_order > last_received + 1:
-        lost_packets += (current_order - last_received - 1)
-    
-    print(f"(client) lost packets: {lost_packets}")
-    print(f"(client) number of packets out of order: {out_of_order}\n")
-    last_received = current_order
+except KeyboardInterrupt:
+    print("(client) Ctrl+C received. Unregistering client.")
+    client_socket.sendto("unregister".encode(), (server_address, 12345))
--- server.py
+++ server.py
@@ -4,19 +4,26 @@ import threading
 import json 
 
 # Função para enviar mensagem para um cliente
-def send_message_to_client(event, client_address, message, server_socket):
+def send_message_to_client(event, client_address, server_socket):
+    print(f"(server) Sending message to client => {client_address}")
+    message = {"count": count, "content": f"Message => {i}"}  # Crie um dicionário para a mensagem
     event.wait()  # Espera pelo sinal para enviar a mensagem
     json_message = json.dumps({"message": message})  # Converta o dicionário para uma string JSON
     server_socket.sendto(json_message.encode(), client_address)  # Codifique a string JSON antes de enviar
-
-# Inicialize o socket UDP
+ 
+# Inicialização do socket UDP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(("0.0.0.0", 12345))  # Escuta em todas as interfaces de rede
 server_socket.setblocking(0)  # Socket não bloqueante
 
+# Conjunto de clientes
 clients = set()
 
+# Mensagem
 i = 0
+# Contador de pacotes totais enviados
+count = 0
+
 while True:
     try:
         # Recebe mensagem e endereço do socket
@@ -25,8 +32,10 @@ while True:
 
         # Registra o cliente no conjunto de clientes
         if message.decode() == "register":
+            print(f"(server) Registering client. {address}")
             clients.add(address)
         elif message.decode() == "unregister":
+            print(f"(server) Unregistering client. {address}")
             clients.discard(address)
 
     except BlockingIOError:
@@ -36,17 +45,19 @@ while True:
     # Preparação para enviar o mesmo pacote para todos os clientes ao mesmo tempo
     event = threading.Event()
     threads = []
-    message_dict = {"count": i, "content": f"Message {i}"}  # Crie um dicionário para a mensagem
-
+    
     for client in clients:
-        thread = threading.Thread(target=send_message_to_client, args=(event, client, message_dict, server_socket))
+        thread = threading.Thread(target=send_message_to_client, args=(event, client, server_socket))
         threads.append(thread)
         thread.start()
 
-    event.set()  # Dispara o sinal para todos os threads enviarem a mensagem
+    # Dispara o sinal para todos os threads enviarem a mensagem
+    event.set()  
 
     for thread in threads:
         thread.join()
 
-    time.sleep(1)  # Intervalo de tempo entre cada mensagem (1 segundo)
+    # Intervalo de tempo entre cada mensagem (1 segundo)
+    time.sleep(1)  
     i += 1
+    count += 1
